62c68c1d7a4d0948c16b6c15b02ce754d2941ed4
Tinuvile <3145593233@qq.com>
2025-12-12 15:40:58 +0800

    init
    Signed-off-by: Tinuvile <3145593233@qq.com>

---
+++ Trend Sonar MVP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trend Sonar MVP
+//
+//  Created by admin on 2025/12/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trend Sonar MVP/Trend_Sonar_MVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trend_Sonar_MVPApp.swift
+//  Trend Sonar MVP
+//
+//  Created by admin on 2025/12/12.
+//
+
+import SwiftUI
+
+@main
+struct Trend_Sonar_MVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
